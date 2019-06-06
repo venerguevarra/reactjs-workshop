57f758cd80cf9ffb203073db637cd929a625d513
Vener Guevarra <vguevarra@citronsolutions.com>
2019-06-06 22:38:51 +0800

    Added reactjs event handling demo

---
+++ react-demo-app-004/src/ShoppingList.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function ShoppingList(props) {
+    return (
+        <div>
+            <div>
+                <h2>Must buy products</h2>
+            </div>
+            <ul>
+                {
+                    props.list.map((item) => {
+                        if (item.active === true) {
+                            return (<li key={item.name}>
+                                <p>
+                                    <span>{item.name}</span>
+                                    <span>
+                                        <button onClick={() => { props.onRemoveItem(item.name) }}>Remove</button>
+                                        <button onClick={() => { props.onDeactivateItem(item.name) }}>Deactivate</button>
+                                    </span>
+                                </p>
+                            </li>
+                            )
+                        }
+                    })
+                }
+            </ul>
+            <div>
+                <h2>Optional products</h2>
+            </div>
+            <ul>
+                {
+                    props.list.map(item => {
+                        if (!item.active) {
+                            return <li key={item.name}>
+                                <p>
+                                    <span> {item.name} </span>
+                                    <span>
+                                        <button onClick={() => props.onRemoveItem(item.name)}>Remove</button>
+                                    </span>
+                                </p>
+                            </li>
+                        }
+                    })
+                }
+            </ul>
+        </div>
+    );
+}
+
+export default ShoppingList;
